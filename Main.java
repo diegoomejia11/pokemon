acfd2aafc98530f5ed5991003f1c3100a5d78b21
Demian31415 <demian314@ciencias.unam.mx>
2024-09-17 11:41:44 -0600

    Plantilla

---
+++ Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
+
+class Pokemon{
+    String nombre;
+    Tipo tipo;
+    Ataque ataque;
+
+    int HP;
+}
+
+class Ataque{
+    String nombre;
+    Tipo tipo;
+
+    int poder;
+    int probabilidad;
+}
+
+class Tipo{
+    String nombre;
+}
