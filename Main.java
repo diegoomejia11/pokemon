f12df379762fbabd900d8e74b13732fe6e70a08f
Demian31415 <demian314@ciencias.unam.mx>
2024-09-17 12:59:21 -0600

    Estructura más compleja

--- Main.java
+++ Main.java
@@ -1,7 +1,36 @@
 public class Main{
+    static Tipo[] tipos = {new Tipo("normal"),
+        new Tipo("fuego"),
+        new Tipo("agua"),
+        new Tipo("planta"),
+        new Tipo("electrico")};
     public static void main(String[] args) {
-        //cosa
-	//cosa23
+        double[][] combate = {{1.0, 1.0, 1.0, 1.0, 1.0},
+                              {1.0, 0.5, 0.5, 2.0, 1.0},
+                              {1.0, 2.0, 0.5, 0.5, 1.0},
+                              {1.0, 0.5, 2.0, 0.5, 1.0},
+                              {1.0, 1.0, 2.0, 0.5, 0.5},
+                            }; 
+
+        
+
+        //Pokemon pikachu = new Pokemon("pikachu",
+        //                              tipos[4],
+        //                              new Ataque("impactrueno",
+        //                                         tipos[4],
+        //                                         20,
+        //                                         30),
+        //                              200);
+    }
+
+    public static int indexOf(String nombre){
+        for (int i = 0; i < 5; i++){
+            if (tipos[i].nombre == nombre){
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
 
@@ -11,6 +40,13 @@ class Pokemon{
     Ataque ataque;
 
     int HP;
+
+    public Pokemon(String nombre, Tipo tipo, Ataque ataque, int HP){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.ataque = ataque;
+        this.HP = HP;
+    }
 }
 
 class Ataque{
@@ -19,8 +55,19 @@ class Ataque{
 
     int poder;
     int probabilidad;
+
+    public Ataque(String nombre, Tipo tipo, int poder, int probabilidad){
+        this.nombre = nombre;
+        this.tipo = tipo;
+        this.poder = poder;
+        this.probabilidad = probabilidad;
+    }
 }
 
 class Tipo{
     String nombre;
+
+    public Tipo(String nombre){
+        this.nombre = nombre;
+    }
 }
