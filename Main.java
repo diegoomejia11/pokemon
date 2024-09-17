1bca9b2c3799cf35d40845c023740201166b554b
Demian31415 <demian314@ciencias.unam.mx>
2024-09-17 14:11:45 -0600

    Pokemones por agregar

--- Main.java
+++ Main.java
@@ -12,7 +12,49 @@ public class Main{
                               {1.0, 1.0, 2.0, 0.5, 0.5},
                             }; 
 
-        
+        // Necesitamos máximo 24 pokemones diferentes de alguno de los 5 tipos
+        // Cada pokemón necesita solo 1 ataque
+
+        // Usen esta página: https://pokemon.fandom.com/wiki/Category:Generation_I_Pok%C3%A9mon
+        // Tiene una lista de los pokemones de primera generación con la info de c/u
+
+        /**
+         *      NORMAL
+         * Rattata
+         * Meowth
+         * Ditto
+         * Eeve
+         * Snorlax
+         * 
+         *      FUEGO
+         * Charmander
+         * Vulpix
+         * Ponyta
+         * Flareon
+         * Moltres
+         * 
+         *      AGUA
+         * Squirtle
+         * Psyduck
+         * Poliwag
+         * Seel
+         * Staryu
+         * Magikarp
+         * Vaporeon
+         * Articuno
+         * 
+         *      PLANTA
+         * Tangela
+         * 
+         *      ELECTRICO
+         * Pikachu
+         * Voltrob
+         * Electrabuzz
+         * Jolteon
+         * Zapdos
+         * 
+         */
+
 
         //Pokemon pikachu = new Pokemon("pikachu",
         //                              tipos[4],
