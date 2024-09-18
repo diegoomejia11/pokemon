7da4507e4afd340c81a042658bb3cc8131ab8486
diegoomejia11 <diegomejiaarroyo11@ciencias.unam.mx>
2024-09-17 20:23:26 -0600

    diego

---
+++ Diego.java
@@ -0,0 +1,12 @@
+import A.Basisc.*;
+public class Diego{
+
+    public statiic void main(String[]args){
+	Pokemon flareon = new Pokemon ("Flareon", new Tipo("Fuego"), new Ataque("Lanzallamas", new Tipo ("Fuego"), 95,100), 65);
+    Pokemon moltres = new Pokemon("Moltres", new Tipo ("Fuego"), new Ataque ("Ala de Acero" , new Tipo ("Fuego"), 70, 100), 90);
+    Pokemon jolteon = new Pokemon ("jolteon", new Tipo ("Electrico"), new Ataque ("Rayo", new Tipo ("Electrico"), 90, 100), 65);
+    Pokemon magikarp = new Pokemon ("Magikarp", new Tipo ("Agua"), new Ataque("Salpicadura", new Tipo ("Agua"), 20, 100), 20);
+   Pokemon articuno = new Pokemon ("Articuno", new Tipo("Agua"), new Ataque("Ventisca", new Tipo("Agua"), 110, 70), 90);
+Pokemon zapdos = new Pokemon("Zapdos", new Tipo("Electrico"), new Ataque("Trueno", new Tipo ("Electrico"),110, 70), 90);
+    }
+}
