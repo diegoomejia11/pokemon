5377157e665bb8c81204e8c775be4b868fdc1a4b
AxelTapiaCC <axel.tapia@ciencias.unam.mx.>
2024-09-17 19:25:18 -0600

    EqupoAxel

---
+++ Axel.java
@@ -0,0 +1,13 @@
+import A.*;
+
+public class Axel {
+    public static void main(String[] args){  Pokemon pika = new Pokemon("pikachu", new Tipo("electrico"), new Ataque("impactrueno", new Tipo("new"), 0, 0), 0);
+        Pokemon ditto = new Pokemon("Ditto", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 0, 0), 0);
+        Pokemon eevee = new Pokemon("Eevee", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 0, 0), 0);
+        Pokemon vulpix = new Pokemon("Vulpix", new Tipo("fuego"), new Ataque("llamarada", new Tipo("fuego"), 0, 0), 0);
+        Pokemon voltrob = new Pokemon("Voltrob", new Tipo("electrico"), new Ataque("rayo", new Tipo("electrico"), 0, 0), 0);
+        Pokemon squirtle = new Pokemon("Squirtle", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 0, 0), 0);
+        Pokemon staryu = new Pokemon("Staryu", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 0, 0), 0);
+        
+    }
+}
