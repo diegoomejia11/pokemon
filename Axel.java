cadebd27fc634f100b7b426e6031122785704642
AxelTapiaCC <axel.tapia@ciencias.unam.mx.>
2024-09-17 19:51:38 -0600

    Axel

--- Axel.java
+++ Axel.java
@@ -1,13 +1,15 @@
 import A.*;
 
 public class Axel {
-    public static void main(String[] args){  Pokemon pika = new Pokemon("pikachu", new Tipo("electrico"), new Ataque("impactrueno", new Tipo("new"), 0, 0), 0);
-        Pokemon ditto = new Pokemon("Ditto", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 0, 0), 0);
-        Pokemon eevee = new Pokemon("Eevee", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 0, 0), 0);
-        Pokemon vulpix = new Pokemon("Vulpix", new Tipo("fuego"), new Ataque("llamarada", new Tipo("fuego"), 0, 0), 0);
-        Pokemon voltrob = new Pokemon("Voltrob", new Tipo("electrico"), new Ataque("rayo", new Tipo("electrico"), 0, 0), 0);
-        Pokemon squirtle = new Pokemon("Squirtle", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 0, 0), 0);
-        Pokemon staryu = new Pokemon("Staryu", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 0, 0), 0);
-        
+    public static void main(String[] args){ 
+        Pokemon[] pokemones = {
+            new Pokemon("Ditto", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 40, 100), 48),
+            new Pokemon("Eevee", new Tipo("normal"), new Ataque("placaje", new Tipo("normal"), 40, 100), 55),
+            new Pokemon("Vulpix", new Tipo("fuego"), new Ataque("llamarada", new Tipo("fuego"), 90, 100), 38),
+            new Pokemon("Voltorb", new Tipo("electrico"), new Ataque("rayo", new Tipo("electrico"), 50, 90),40),
+            new Pokemon("Squirtle", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 120, 80), 44),
+            new Pokemon("Staryu", new Tipo("agua"), new Ataque("hidrobomba", new Tipo("agua"), 120, 80), 30)
+            
+        };
     }
 }
